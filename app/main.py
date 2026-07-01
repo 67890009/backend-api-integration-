53efe6ff363727512b4d0ba5e4338a5c1a993690
sidhuedara <sidhuedara@gmail.com>
2026-07-01 09:25:48 +0530

    Add initial database migration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from sqlalchemy import text
 
-app = FastAPI(
-    title="Employee Leave Management System",
-    version="1.0.0",
-)
+from app.db.session import AsyncSessionLocal
+
+app = FastAPI()
+
 
 @app.get("/")
 async def root():
+
+    async with AsyncSessionLocal() as session:
+        result = await session.execute(text("SELECT 1"))
+
     return {
-        "message": "Employee Leave Management System API"
+        "database": result.scalar()
     }
