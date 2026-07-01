e04c854a1a92f975280833a6d992569edc6efcc6
Surya Karuturi <yourgithubemail@example.com>
2026-07-01 15:59:08 +0530

    Implement Leave Types module

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from sqlalchemy import text
+from app.leave_types.router import router as leave_type_router
 from app.auth.router import router as auth_router
 from app.core.config import settings
 from app.db.session import AsyncSessionLocal
 import app.db.model
 app = FastAPI()
 
+app.include_router(leave_type_router)
 app.include_router(auth_router)
 @app.get("/")
 async def root():
